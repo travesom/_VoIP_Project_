0bbdeb122fe1192942418d83abb483c22fab8a2d
zlociu <zlociu@onet.eu>
2020-03-14 18:05:14 +0100

    add protocol code

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ Project of VoIP communicator, contains client app and server
 
 
 ## Used Technology
-> C# \
-> C#
+> for serwer app: .NET Framework 4.7
+> for klient app: .NET Framework 4.7
 
 ## Addition
 
---
+++ Telefon_serwer/Telefon_serwer/VNCP.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Telefon_serwer
+{
+    /// <summary>
+    /// Voice Network Control Protocol. 
+    /// <para></para>
+    /// Use for communication client-server.
+    /// </summary>
+
+    /// <remarks>
+    /// Communication Protocol:
+    ///                8               16              24              32
+    ///  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+    /// |                                                               |
+    /// |_ _ _ _ _ _ _ _|_ _ _ _ _ _ _ _|_ _ _ _ _ _ _ _|_ _ _ _ _ _ _ _|
+    /// |                    Destination IP Address                     |
+    /// |_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _|
+    /// |                      Source IP Address                        |
+    /// |_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _|
+    /// |                       (optional) DATA                         |
+    /// |                                                               |
+    /// |_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _|
+    /// 
+    /// </remarks>
+    class VNCP
+    {
+        private IPAddress source;
+        private IPAddress destination;
+        private string Data;
+    }
+}
