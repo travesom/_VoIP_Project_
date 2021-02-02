2e7695fee4c39dd46ec701ae49aece3f7fa44657
travesom <travesom@gmail.com>
2021-02-02 05:46:02 +0100

    Revert "ssl_test"
    This reverts commit 510a5c80daaac60148a54e66eefb8a0dd473fa9a.

--- Telefon_klient/Telefon_klient/Program.cs
+++ Telefon_klient/Telefon_klient/Program.cs
@@ -4,32 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-
 namespace Telefon_klient
 {
     static class Program
-    {   
+    {
         /// <summary>
         /// Główny punkt wejścia dla aplikacji.
         /// </summary>
         [STAThread]
-        static void Main(string[] args)
+        static void Main()
         {
-            
-            if (args == null || args.Length < 1)
-            {
-                
-            }
-            else
-            Form1.machineName = args[0];
-            if (args.Length < 2)
-            {
-                Form1.serverName = Form1.machineName;
-            }
-            else
-            {
-                Form1.serverName = args[1];
-            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
